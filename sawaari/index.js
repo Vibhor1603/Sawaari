bb8554b00014b73cc146a38200f82397436fde27
Vibhor Sharma <vibhorsharmak@gmail.com>
2025-08-01 18:47:47 +0530

    added frontend deployed link for CORS

--- sawaari/index.js
+++ sawaari/index.js
@@ -37,6 +37,9 @@ app.use(
       "http://localhost:5173", // Vite default port
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5173",
+      "https://sawaari.vercel.app/",
+      "https://sawaari-vibhor-sharmas-projects.vercel.app/",
+      "https://sawaari-nums1xo72-vibhor-sharmas-projects.vercel.app/",
     ],
     credentials: true,
   })
